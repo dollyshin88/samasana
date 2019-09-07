4c7bd61c5557d7ec413369fdb00892ed08d8ceb1
dollyshin <dollyshin88@gmail.com>
2019-09-06 20:13:12 -0700

    set up static pages controller, root route and root view to test the entry jsx file

---
+++ frontend/asana.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import React from 'react'
+// import { configureStore } from './store/store';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById('root');
+
+    ReactDOM.render(<h2>placeholder</h2>, root);
+
+});
