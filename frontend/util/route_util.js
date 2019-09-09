196388b9231904d9775c0baa674f22e6a0b6954d
dollyshin <dollyshin88@gmail.com>
2019-09-08 23:07:47 -0700

    Create utilities for session api and auth and protected routes

---
+++ frontend/util/route_util.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const Auth = ({ path, component: Component, loggedIn, exact }) => (
+    <Route path={path} exact={exact} render={ props => (
+        !loggedIn ? (
+            <Component {...props} />
+        ) : (
+            <Redirect to='/home' />
+        )
+    )} />
+);
+
+const Protected = ({ path, component: Component, loggedIn, exact }) => {
+    console.log(loggedIn);
+    return (
+    <Route path={path} exact={exact} render={ props => (
+        loggedIn ? (
+            <Component {...props} />
+        ) : (
+            <Redirect to='/samasana' />
+        )
+    )} />
+)};
+
+const mapStateToProps = (state) => ({
+    loggedIn: Boolean(state.session.id),
+});
+
+export const AuthRoute = withRouter(connect(mapStateToProps)(Auth));
+export const ProtectedRoute = withRouter(connect(mapStateToProps)(Protected));
