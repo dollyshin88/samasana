8db2cdab5ee614aed9a447f57b7dcfdfdc172b82
dollyshin <dollyshin88@gmail.com>
2019-09-12 15:49:25 -0700

    Add projects reducer and minor fixes to related acitons and projects json view

---
+++ frontend/reducers/projects_reducer.js
@@ -0,0 +1,23 @@
+import { RECEIVE_ALL_PROJECTS, RECEIVE_PROJECT, REMOVE_PROJECT } from '../actions/project_actions';
+
+const projectsReducer = (state={}, action) => {
+    Object.freeze(state);
+
+    switch (action.type) {
+        case RECEIVE_ALL_PROJECTS:
+            
+            return action.projects;
+        case RECEIVE_PROJECT:
+            return Object.assign({}, state, action.payload.project);
+
+        case REMOVE_PROJECT:
+            const nextState = Object.assign({}, state)
+            delete nextState.projects[action.projectId]
+            return nextState;
+
+        default:
+            return state;
+    }
+};
+
+export default projectsReducer;
