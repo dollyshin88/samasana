9bd440badbfa7a9d0c2e7081568887aaea0cc7d8
dollyshin <dollyshin88@gmail.com>
2019-09-12 13:32:29 -0700

    Add ajax utility funcitons for projects

---
+++ frontend/util/project_api_util.js
@@ -0,0 +1,36 @@
+export const fetchAllProjects = workspace_id => (
+    $.ajax({
+        method: 'GET',
+        url: `/api/workspaces/${workspace_id}/projects`
+    })
+);
+
+export const fetchProject = id => (
+    $.ajax({
+        method: 'GET',
+        url: `/api/projects/${id}`
+    })
+);
+
+export const createProject = project => (
+    $.ajax({
+        method: 'POST', 
+        url: `/api/projects`,
+        data: { project }
+    })
+); 
+
+export const updateProject = project => (
+    $.ajax({
+        method: 'PATCH',
+        url: `/api/projects/${project.id}`,
+        data: { project }
+    })
+);
+
+export const deleteProject = project => (
+    $.ajax({
+        method: 'DELETE',
+        url: `/api/projects/${project.id}`
+    })
+);
