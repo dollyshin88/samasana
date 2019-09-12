c31630918180a8cff0d074d0f4efb8c6185892ef
dollyshin <dollyshin88@gmail.com>
2019-09-12 15:35:33 -0700

    Add new action and reducer to set current workspace in the state

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import workspacesReducer from './workspaces_reducer';
 import membersReducer from './members_reducer';
+import currentWorkspaceReducer from './current_workspace_reducer';
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
+    currentWorkspace: currentWorkspaceReducer,
     workspaces: workspacesReducer,
     members: membersReducer,
 });
