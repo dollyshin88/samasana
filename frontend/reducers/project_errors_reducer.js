be73231caf3c5fa112838a99565c55eb234a339a
dollyshin <dollyshin88@gmail.com>
2019-09-13 08:46:44 -0700

    Minor typo fix

--- frontend/reducers/project_errors_reducer.js
+++ frontend/reducers/project_errors_reducer.js
@@ -1,4 +1,4 @@
-import { RECEIVE_PROJECT_ERRORS, RECEIVE_PROJECT } from '../actions/task_actions';
+import { RECEIVE_PROJECT_ERRORS, RECEIVE_PROJECT } from '../actions/project_actions';
 
 const projectErrorsReducer = (state=[], action) => {
     Object.freeze(state);
