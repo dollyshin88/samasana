a316dd1c1b9befb5e70e43cfed9d92e508138629
dollyshin <dollyshin88@gmail.com>
2019-09-12 18:32:13 -0700

    Fix bug - change how current workspace is being set in the state

--- frontend/reducers/current_workspace_reducer.js
+++ frontend/reducers/current_workspace_reducer.js
@@ -1,15 +1,21 @@
-import { SET_CURRENT_WORKSPACE } from '../actions/workspace_actions';
+import { RECEIVE_NEW_WORKSPACE } from '../actions/workspace_actions';
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../actions/session_actions'; 
 
 const currentWorkspaceReducer = (state={}, action) => {
     Object.freeze(state);
 
     switch (action.type) {
-        case SET_CURRENT_WORKSPACE:
-            return action.workspace;
-    
+        case RECEIVE_NEW_WORKSPACE:
+            return Object.values(action.payload.workspaces)[0];
+        case RECEIVE_CURRENT_USER:
+            return Object.values(action.payload.workspaces)[0]; 
+        case LOGOUT_CURRENT_USER:
+            return {};
         default:
             return state;
     }
 };
 
 export default currentWorkspaceReducer;
+
+//when receive_current_user or when receive_new_workspace, set current workspace
