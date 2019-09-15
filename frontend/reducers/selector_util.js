a049dc7e31345850177b48e49e66e8ff32be75ae
dollyshin <dollyshin88@gmail.com>
2019-09-15 12:45:21 -0700

    Add selector util functions to grab sections and tasks specific to a project

--- frontend/reducers/selector_util.js
+++ frontend/reducers/selector_util.js
@@ -7,3 +7,11 @@ export const initialsSelector = name => {
         return splitName[0][0] + splitName[1][0];
     }
 };
+
+export const projectTasksSelector = (state, projectId) => {
+    return Object.values(state.entities.tasks).filter(task => task.project_id === projectId);
+};
+
+export const projectSectionsSelector = (state, projectId) => {
+    return Object.values(state.entities.sections).filter(section => section.project_id === projectId);
+};
