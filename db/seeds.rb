6ece052ae9009e963978f4832bbfd8a6f80006aa
dollyshin <dollyshin88@gmail.com>
2019-09-16 00:14:38 -0700

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,51 +11,48 @@ User.destroy_all
 Task.destroy_all
 Project.destroy_all
 Workspace.destroy_all
+
 ActiveRecord::Base.connection.reset_pk_sequence!('users')
 ActiveRecord::Base.connection.reset_pk_sequence!('projects')
 ActiveRecord::Base.connection.reset_pk_sequence!('tasks')
 ActiveRecord::Base.connection.reset_pk_sequence!('workspace_memberships')
 
 
-User.create([
-    {name: 'Ernie Man', email:'ernie@test.com', password: 'password'},
-    {name: 'Andrew', email:'andrew@test.com', password: 'password'},
-    {name: 'Owen', email:'owem@test.com', password: 'password'},
-    {name: 'Alia Shafi', email:'alia@test.com', password: 'password'},
-])
-Workspace.create([
-    {name: 'Workspace One'},
-    {name: 'Workspace Two'},
-    {name: 'Workspace Three'},
-])
-WorkspaceMembership.create([
-    {member_id: 1, workspace_id: 1, is_admin: true},
-    {member_id: 3, workspace_id: 1, is_admin: false},
-    {member_id: 2, workspace_id: 2, is_admin: true},
-    {member_id: 4, workspace_id: 3, is_admin: true},
-    {member_id: 1, workspace_id: 3, is_admin: false},
-    {member_id: 2, workspace_id: 3, is_admin: false},
-])
 
-Project.create([
-    {name: 'Develop Hipcamp', owner_id: 1, workspace_id: 1},
-    {name: 'Develop Strava', owner_id: 4, workspace_id: 3},
-    {name: 'MERN Stack', owner_id: 4, workspace_id: 3},
-    {name: 'Hipcamp Fullstack', owner_id: 2, workspace_id: 2},
-])
+user1 = User.create({name: 'Ernie Man', email:'ernie@test.com', password: 'password'})
+workspace1 = Workspace.create({name: 'Workspace One'})
+wk_mem1 = WorkspaceMembership.create({member_id: user1.id, workspace_id: workspace1.id, is_admin: true})
+
+user2 = User.create({name: 'Alia Shafi', email:'alia@test.com', password: 'password'})
+workspace2 = Workspace.create({name: 'Workspace Two'})
+wk_mem2 = WorkspaceMembership.create({member_id: user2.id, workspace_id: workspace2.id, is_admin: true})
+
+user3 = User.create({name: 'Owen', email:'owem@test.com', password: 'password'})
+workspace3 = {name: 'Workspace Three'}
+wk_mem3 = WorkspaceMembership.create({member_id: user3.id, workspace_id: workspace3.id, is_admin: true})
+
+wk_mem4 = WorkspaceMembership.create({member_id: user1.id, workspace_id: workspace2.id, is_admin: false})
+wk_mem5 = WorkspaceMembership.create({member_id: user3.id, workspace_id: workspace2.id, is_admin: false})
+
+
+project1 = Project.create({name: 'Develop Hipcamp', owner_id: user1.id, workspace_id: workspace1.id})
+project2 = Project.create({name: 'Develop Strava', owner_id: user2.id, workspace_id: workspace2.id})
+project3 = Project.create({name: 'MERN Stack', owner_id: user2.id, workspace_id: workspace2.id})
+project4 = Project.create({name: 'Depop Fullstack', owner_id: user3.id, workspace_id: workspace3.id})
+
 
 Task.create([
-    {name: 'Create Design Docs', creator_id: 4, workspace_id: 3, project_id: 2, due_on: '2019-09-01'},
-    {name: 'Complete backend for feature one', creator_id: 4, workspace_id: 3, project_id: 2, due_on: '2019-09-03'},
-    {name: 'Complete CSS Strava', creator_id: 4, workspace_id: 3, project_id: 2, due_on: '2019-09-20'},
-    {name: 'Setup AWS', creator_id: 4, workspace_id: 3, project_id: 2, due_on: '2019-09-17'},
-    {name: 'Design Wireframes', creator_id: 4, workspace_id: 3, project_id: 3, due_on: '2019-09-25'},
-    {name: 'Complete Route Plans', creator_id: 4, workspace_id: 3, project_id: 3, due_on: '2019-09-26'},
-    {name: 'Build out backend database', creator_id: 4, workspace_id: 3, project_id: 3, due_on: '2019-09-28'},
+    {name: 'Create Design Docs', creator_id: user2.id, workspace_id: workspace2.id, project_id: project2.id, due_on: '2019-09-01'},
+    {name: 'Complete backend for feature one', creator_id: user2.id, workspace_id: 3, project_id: project2.id, due_on: '2019-09-03'},
+    {name: 'Complete CSS Strava', creator_id: user2.id, workspace_id: workspace2.id, project_id: project2.id, due_on: '2019-09-20'},
+    {name: 'Setup AWS', creator_id: user2.id, workspace_id: workspace2.id, project_id: project2.id, due_on: '2019-09-17'},
+    {name: 'Design Wireframes', creator_id: user2.id, workspace_id: workspace2.id, project_id: project3.id, due_on: '2019-09-25'},
+    {name: 'Complete Route Plans', creator_id: user2.id, workspace_id: workspace2.id, project_id: project3.id, due_on: '2019-09-26'},
+    {name: 'Build out backend database', creator_id: user2.id, workspace_id: workspace2.id, project_id: project3.id, due_on: '2019-09-28'},
 
-    {name: 'Create Design Docs', creator_id: 1, workspace_id: 1, project_id: 1, due_on: '2019-09-16'},
-    {name: 'Complete backend for feature one', creator_id: 1, workspace_id: 1, project_id: 1, due_on: '2019-09-03'},
-    {name: 'Complete CSS for Hipcamp', creator_id: 1, workspace_id: 1, project_id: 1, due_on: '2019-09-20'},
-    {name: 'Setup AWS', creator_id: 1, workspace_id: 1, project_id: 1, due_on: '2019-09-18'},
+    {name: 'Create Design Docs', creator_id: user1.id, workspace_id: workspace1.id, project_id: project1.id, due_on: '2019-09-16'},
+    {name: 'Complete backend for feature one', creator_id: user1.id, workspace_id: workspace1.id, project_id: project1.id, due_on: '2019-09-03'},
+    {name: 'Complete CSS for Hipcamp', creator_id: user1.id, workspace_id: workspace1.id, project_id: project1.id, due_on: '2019-09-20'},
+    {name: 'Setup AWS', creator_id: user1.id, workspace_id: workspace1.id, project_id: project1.id, due_on: '2019-09-18'},
 
 ])
