a6a4b68aa2d0bd5d78233189170c0bc7b8ee850c
dollyshin <dollyshin88@gmail.com>
2019-10-24 11:01:54 +0900

    Temp fix for signup flow redirecting to workspace home page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Modal from './modal/modal';
 import CompanyHome from './company_home/home';
 import WorkspaceContainer from './workspace/workspace_container';
-import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { AuthRoute, SetupRoute, ProtectedRoute } from '../util/route_util';
 import Welcome from './session/welcome';
 import SignupProfileForm from './session/signup_container';
 import Setup from './setup/setup';
@@ -18,10 +18,11 @@ function App(props) {
             <Switch>
                 <AuthRoute exact path='/samasana' component={CompanyHome} />
                 <AuthRoute exact path='/signup/welcome' component={Welcome} />
-                <AuthRoute exact path='/signup/profile' component={SignupProfileForm} />
+                <SetupRoute exact path='/signup/profile' component={SignupProfileForm} />
                 
                 <ProtectedRoute path='/setup' component={Setup} />
                 <ProtectedRoute path='/workspace' component={WorkspaceContainer} />
+                <ProtectedRoute path='/' component={WorkspaceContainer} />
 
                 <Route component={ErrorPage} />
             </Switch>
--- frontend/components/workspace/workspace.jsx
+++ frontend/components/workspace/workspace.jsx
@@ -45,12 +45,11 @@ function Workspace(props){
     }
 
     useEffect(() => {
-        props.fetchAllWorkspaces();
-        // const sessionRegex = new RegExp('\/setup.*');
-        // if (!sessionRegex.test(props.location.pathname)) {
-            
+        const sessionRegex = new RegExp('\/workspace.*');
+        if (sessionRegex.test(props.location.pathname)) {
+            props.fetchAllWorkspaces();
             props.fetchWorkspace(props.currentWorkspace.id);
-        // }
+        }
 
     }, []);
 
